c7c90b1dac79a3aa9a10ded89828866df7bdac08
Madhava Krishna <madhava20217@iiitd.ac.in>
2024-04-21 06:49:29 +0530

    regvlm added

--- utils/RegVLM.py
+++ utils/RegVLM.py
@@ -1,5 +1,6 @@
 import torch
 import copy
+import math
 
 
 def deleteEncodingLayers(model, num_layers_to_keep):  # must pass in the full bert model
@@ -89,9 +90,15 @@ class RegVLM(torch.nn.Module):
                  vocab_size = 30522,
                  mask_token_id = 103,
                  cls_token_id = 101,
-                 tau = None):
+                 tau = None,
+                 lamda = 25,
+                 mu = 25,
+                 nu = 1.,
+                 eps = 1e-4):
        super().__init__()
-       self.vit = vit
+       self.vit = vit.vit
+       self.mim_reconstruction_joint = vit.decoder
+       self.mim_reconstruction = copy.deepcopy(vit.decoder)
        self.bert = bert.base_model
        self.bert = deleteEncodingLayers(self.bert.base_model, bert_layers)
        self.fusion = RegVLM_Mixer(bert.base_model,
@@ -101,6 +108,11 @@ class RegVLM(torch.nn.Module):
                               emb_dims = bert_emb_dim,
                               cls_token_id = cls_token_id)
        
+       self.lamda = lamda
+       self.mew = mu
+       self.nu = nu
+       self.eps = eps
+              
        # vit patches data
        self.vit_num_patches = vit_num_patches
        
@@ -137,6 +149,7 @@ class RegVLM(torch.nn.Module):
        )
         
        # head for masked language modeling
+       self.mlm_head_joint = copy.deepcopy(bert.cls)
        self.mlm_head = bert.cls
        
        self.itc_head = torch.nn.Linear(bert_emb_dim, bert_emb_dim)
@@ -150,7 +163,7 @@ class RegVLM(torch.nn.Module):
        
        
     def forward(self, image, text, attn_mask,
-                masked_image = None,
+                masked_pos = None,
                 masked_text = None,
                 image_text_matching = False,
                 retrieval = False,
@@ -178,7 +191,7 @@ class RegVLM(torch.nn.Module):
             img_rep = self.vit(image)['last_hidden_state']              # clean image
             txt_rep = self.bert(text, attn_mask)['last_hidden_state']   # clean text
             
-            mask_img_rep = self.vit(masked_image)['last_hidden_state']
+            mask_img_rep = self.vit(image, bool_masked_pos = masked_pos)['last_hidden_state']
             mask_txt_rep = self.bert(masked_text, attn_mask)['last_hidden_state']
             
             # multimodal prediction
@@ -192,31 +205,71 @@ class RegVLM(torch.nn.Module):
             img_txt_joint = self.fusion(img_rep, txt_rep, attn_mask)['last_hidden_state']
         
             return (c_img_m_txt, m_img_c_txt, img_txt_joint, mask_img_rep, txt_prediction, img_rep, txt_rep)
-    
-    def get_mrm_loss(self, online_representation, target_representation, mask):
-        # remove cls token
-        on_rep = torch.nn.functional.normalize(self.mrm_proj(online_representation[:, 1:, :]), dim = -1)
-        tr_rep = torch.nn.functional.normalize(self.mrm_proj(target_representation[:, 1:, :]), dim = -1)
 
-        
-        loss = torch.nn.functional.mse_loss(on_rep, tr_rep, reduction = 'none')
-        mrm_loss = (loss * mask).sum()/(mask.sum() + 1e-5)              # add for 0 division errors
-        return mrm_loss
+
+    def get_mim_loss(self, img_rep, img, bool_masked_pos):
+        sequence_output = img_rep
+
+        # Reshape to (batch_size, num_channels, height, width)
+        sequence_output = sequence_output[:, 1:]
+        batch_size, sequence_length, num_channels = sequence_output.shape
+        height = width = math.floor(sequence_length**0.5)
+        sequence_output = sequence_output.permute(0, 2, 1).reshape(batch_size, num_channels, height, width)
+
+        # Reconstruct pixel values
+        reconstructed_pixel_values = self.mim_reconstruction(sequence_output)
+
+        masked_im_loss = None
+        if bool_masked_pos is not None:
+            size = 224 // 16
+            bool_masked_pos = bool_masked_pos.reshape(-1, size, size)
+            mask = (
+                bool_masked_pos.repeat_interleave(16, 1)
+                .repeat_interleave(16, 2)
+                .unsqueeze(1)
+                .contiguous()
+            )
+            reconstruction_loss = torch.nn.functional.l1_loss(img, reconstructed_pixel_values, reduction="none")
+            masked_im_loss = (reconstruction_loss * mask).sum() / (mask.sum() + 1e-5) / 3
+
+        return masked_im_loss
     
-    def get_mim_loss(self, online_representation, target_representation, mask):
-        on_rep = self.mim_proj(online_representation[:, 1:self.vit_num_patches+1, :]) # omit cls token
-        tr_rep = target_representation[:, 1:self.vit_num_patches+1, :]
-        
-        # # normalize
-        on_rep = torch.nn.functional.normalize(on_rep, dim = 2)
-        tr_rep = torch.nn.functional.normalize(tr_rep, dim = 2)
-        
-        loss = torch.nn.functional.l1_loss(on_rep, tr_rep, reduction = 'none')
-        if mask.ndim == 2:
-            mask = mask[:, :, None]
-        mim_loss = (loss * mask).sum() / (mask.sum() + 1e-5)
-        return mim_loss 
+    def get_joint_mim_loss(self, joint_rep, img, bool_masked_pos):
+        joint_rep = joint_rep[:, :self.vit_num_patches+1, :]
+        joint_rep = self.mim_proj(joint_rep)                    # bs x vit_npatch x vit dim
+        sequence_output = joint_rep
+
+        # Reshape to (batch_size, num_channels, height, width)
+        sequence_output = sequence_output[:, 1:]
+        batch_size, sequence_length, num_channels = sequence_output.shape
+        height = width = math.floor(sequence_length**0.5)
+        sequence_output = sequence_output.permute(0, 2, 1).reshape(batch_size, num_channels, height, width)
+
+        # Reconstruct pixel values
+        reconstructed_pixel_values = self.mim_reconstruction_joint(sequence_output)
+
+        masked_im_loss_joint = None
+        if bool_masked_pos is not None:
+            size = 224 // 16
+            bool_masked_pos = bool_masked_pos.reshape(-1, size, size)
+            mask = (
+                bool_masked_pos.repeat_interleave(16, 1)
+                .repeat_interleave(16, 2)
+                .unsqueeze(1)
+                .contiguous()
+            )
+            reconstruction_loss = torch.nn.functional.l1_loss(img, reconstructed_pixel_values, reduction="none")
+            masked_im_loss_joint = (reconstruction_loss * mask).sum() / (mask.sum() + 1e-5) / 3
+
+        return masked_im_loss_joint
     
+    
+    def get_joint_mlm_loss(self, joint_reps, sen, masked_sen):
+        embs = joint_reps[:, self.vit_num_patches+1:, :]
+        scores = self.mlm_head_joint(torch.nn.functional.leaky_relu(embs))
+        labels = torch.where(masked_sen == self.mask_token_id, sen, -100)
+        loss = torch.nn.functional.cross_entropy(scores.view(-1, self.vocab_size), labels.view(-1), ignore_index=-100)
+        return loss
     
     def get_mlm_loss(self, scores, sen, masked_sen):
         labels = torch.where(masked_sen == self.mask_token_id, sen, -100)
@@ -245,9 +298,29 @@ class RegVLM(torch.nn.Module):
         return sim, (loss_i2t+loss_t2i)/2.0
     
     def get_samples(self, similarities):
-        probs = torch.nn.functional.softmax(similarities, dim = 1)
+        probs = torch.nn.functional.softmax(similarities, dim = 1) + 1e-8       # term to make nonnegative
         probs = probs.fill_diagonal_(0)         # eliminate full samples
+        
         txt_indices = torch.multinomial(probs, num_samples=1, replacement=True).squeeze(1)
         img_indices = torch.multinomial(probs.T, num_samples=1, replacement=True).squeeze(1)
         
         return txt_indices, img_indices
+    
+    def get_vicreg_loss(self, features):
+        #Source: https://arxiv.org/pdf/2105.04906.pdf
+              
+        # Calculate invariance -- not required for now as we're only using a part of vicreg
+        # invariance_loss = torch.nn.functional.mse_loss(img_txt_feats, img_txt_feats_prime)
+
+        # Calculate variance of img_txt_feats using torch.var
+        features = torch.nn.functional.adaptive_avg_pool1d(features.transpose(1,2), 1).flatten(1)
+        variance = torch.var(features, dim=0)
+        std = torch.sqrt(variance + self.eps)
+        std_loss = torch.mean(torch.relu(1 - std))
+
+        # Calculate covariance of img_txt_feats using torch after centering the data
+        centered_feats = features - torch.mean(features, dim=0)
+        covariance = torch.mm(centered_feats.T, centered_feats) / (features.shape[0] - 1)        # Get off diagonal elements of covariance
+        cov_loss = torch.sum(torch.pow(covariance - torch.diag(torch.diag(covariance)),2)) / (features.shape[1])
+
+        return  + self.mew * std_loss + self.nu * cov_loss
